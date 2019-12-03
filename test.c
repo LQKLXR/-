1073b85d99316099884f2abdccf4f24f918d4506
LQKLXR <57283912+LQKLXR@users.noreply.github.com>
2019-12-03 13:24:29 +0800

    第一次上机

---
+++ test.c
@@ -0,0 +1,61 @@
+int main()
+{
+	int i;
+	int j;
+	int m;
+	int n;
+	int x;
+    int length = 2;
+	int width = -1;
+	int area = length * width;
+	int div = length / width;
+	
+	float height = 2.1;
+	float deepth = 1.2;
+	
+	length ++;
+	for(i = 0; i < 10; i ++ )
+	{
+		m = 0;
+		for(j = 0; j < 10; j++)
+		{
+			n = 0;
+		}
+	}
+	
+	
+	while(i < 100)
+	{
+		x = 0;
+	}
+	
+	if(height > 2.0)
+	{
+		char symbol1 = '!';
+		symbol1 = 'a';
+		
+	}
+	else if(height <= 2.0 && height >1.0)
+	{
+		char symbol2 = '?';
+		symbol2 = 'b';
+		if(1 > 0)
+		{
+			int lala = 5;
+		}
+		else if (0 <= 1)
+		{
+			int haha = 8;
+		}
+	}
+	else
+	{
+		char symbol3 = '?';
+		symbol3 = 'b';
+	}
+
+	
+	
+	
+    return 0;
+}
